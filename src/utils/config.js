d717f6056f3a26b9ddd0cd1122930a8230eaced7
Jon Bach <bach4@hawaii.edu>
2024-07-03 14:35:25 -1000

    Update config.js
    Added client Id

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  client_id: "<YOUR_CLIENT_ID>",
+  client_id: "7e8f753b3f4a8a8af90aedf5136e73a8",
   server_url:
     "https://us-central1-livechat-experiments.cloudfunctions.net/restApi",
   account_url: "https://accounts.livechatinc.com/"
